ea5731a3777c5a3d113bb0b32551cf42542f58fa
jackyAkagami <jackyrahmatul02@gmail.com>
2024-12-17 11:59:09 +0700

    commit8_2jam47menit41det

---
+++ src/components/admin-panel/ProductForm.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { setLoading } from "@/redux/features/loadingSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import { makeToast } from "@/utils/helper";
+import axios from "axios";
+import Image from "next/image";
+import React, { FormEvent, useState } from "react";
+
+interface IPayload {
+  imgSrc: null | string;
+  fileKey: null | string;
+  name: string;
+  category: string;
+  price: string;
+}
+
+const ProductForm = () => {
+  const [payLoad, setpayLoad] = useState<IPayload>({
+    imgSrc: null,
+    fileKey: null,
+    name: "",
+    category: "",
+    price: "",
+  });
+
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(setLoading(true));
+
+    axios
+      .post("/api/add_product", payLoad)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .then((res) => {
+        makeToast("Product added Succesffully");
+        setpayLoad({
+          imgSrc: null,
+          fileKey: null,
+          name: "",
+          category: "",
+          price: "",
+        });
+      })
+      .catch((err) => console.log(err))
+      .finally(() => dispatch(setLoading(false)));
+  };
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <Image
+        className="max-h-[300px] w-auto object-contain rounded-md"
+        src={payLoad.imgSrc ? payLoad.imgSrc : "/placeholder.jpg"}
+        width={800}
+        height={500}
+        alt="product_image"
+      />
+    </form>
+  );
+};
+
+export default ProductForm;
